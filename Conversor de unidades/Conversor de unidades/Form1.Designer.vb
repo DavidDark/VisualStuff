4e85baa51e5149c63530077177cab3eaa26e98af
David Dark <32287340+DavidDark@users.noreply.github.com>
2019-02-06 22:59:48 -0600

    Add files via upload

---
+++ Conversor de unidades/Conversor de unidades/Form1.Designer.vb
@@ -0,0 +1,192 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.CALCULAR = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Cantidad = New System.Windows.Forms.TextBox()
+        Me.Resultado = New System.Windows.Forms.TextBox()
+        Me.BORRAR = New System.Windows.Forms.Button()
+        Me.SALIR = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'CALCULAR
+        '
+        Me.CALCULAR.Location = New System.Drawing.Point(39, 232)
+        Me.CALCULAR.Name = "CALCULAR"
+        Me.CALCULAR.Size = New System.Drawing.Size(202, 37)
+        Me.CALCULAR.TabIndex = 0
+        Me.CALCULAR.Text = "CALCULAR"
+        Me.CALCULAR.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(32, 145)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(368, 37)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "RESULTADO EN °C/°K"
+        '
+        'Cantidad
+        '
+        Me.Cantidad.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Cantidad.Location = New System.Drawing.Point(437, 66)
+        Me.Cantidad.Name = "Cantidad"
+        Me.Cantidad.Size = New System.Drawing.Size(280, 31)
+        Me.Cantidad.TabIndex = 3
+        '
+        'Resultado
+        '
+        Me.Resultado.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Resultado.Location = New System.Drawing.Point(437, 151)
+        Me.Resultado.Name = "Resultado"
+        Me.Resultado.Size = New System.Drawing.Size(280, 31)
+        Me.Resultado.TabIndex = 4
+        '
+        'BORRAR
+        '
+        Me.BORRAR.Location = New System.Drawing.Point(39, 290)
+        Me.BORRAR.Name = "BORRAR"
+        Me.BORRAR.Size = New System.Drawing.Size(202, 36)
+        Me.BORRAR.TabIndex = 5
+        Me.BORRAR.Text = "BORRAR"
+        Me.BORRAR.UseVisualStyleBackColor = True
+        '
+        'SALIR
+        '
+        Me.SALIR.Location = New System.Drawing.Point(39, 350)
+        Me.SALIR.Name = "SALIR"
+        Me.SALIR.Size = New System.Drawing.Size(81, 54)
+        Me.SALIR.TabIndex = 6
+        Me.SALIR.Text = "SALIR"
+        Me.SALIR.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Controls.Add(Me.RadioButton1)
+        Me.GroupBox1.Location = New System.Drawing.Point(294, 232)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(243, 158)
+        Me.GroupBox1.TabIndex = 7
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Opciones"
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(27, 77)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(76, 17)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "De °K a °C"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(27, 45)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(76, 17)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "De °C a °K"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(32, 60)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(291, 37)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Cantidad en °C/°K"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(723, 72)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(82, 25)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Grados"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(723, 157)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(82, 25)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Grados"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(812, 446)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.SALIR)
+        Me.Controls.Add(Me.BORRAR)
+        Me.Controls.Add(Me.Resultado)
+        Me.Controls.Add(Me.Cantidad)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.CALCULAR)
+        Me.Name = "Form1"
+        Me.Text = "CONVERSOR DE UNIDADES"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents CALCULAR As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Cantidad As TextBox
+    Friend WithEvents Resultado As TextBox
+    Friend WithEvents BORRAR As Button
+    Friend WithEvents SALIR As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Conversor de unidades/Conversor de unidades/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles CALCULAR.Click
+        If RadioButton1.Checked Then 'Abre una condición
+            Resultado.Text = (CDbl(Cantidad.Text + 273)) 'Si está seleccionado el RadioButton1, suma 273
+        ElseIf RadioButton2.Checked Then
+            Resultado.Text = (CDbl(Cantidad.Text - 273)) 'Si está seleccionado el RadioButton2, resta 273
+        End If 'Cierra la condición
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles BORRAR.Click
+        Cantidad.Text = "" 'El cuadro de texto N (El del número) se borra
+        Resultado.Text = "0" 'El cuadro de texto R (El del resultado) se pone en 0
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles SALIR.Click
+        End 'Se cierra completamente el programa
+    End Sub
+End Class
